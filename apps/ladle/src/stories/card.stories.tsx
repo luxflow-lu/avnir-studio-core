8eeb9be427371915030950f3c17d83bb8cdee273
Maxime Zaoui <maximezaoui@MBP-de-Maxime.fritz.box>
2025-10-19 19:35:10 +0200

    chore(ladle): remove legacy stories importing @avnir/ui; will replace with local component stories under apps/ladle/src/components

--- apps/ladle/src/stories/card.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from "@avnir/ui";
-
-export default { title: "Primitives/Card" };
-
-export const Basic = () => (
-  <div className="p-6 bg-[var(--bg)] text-white">
-    <Card>Contenu de carte</Card>
-  </div>
-);
